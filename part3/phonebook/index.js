51ce4c86f2be0eed4511a61a14201868f1868797
sthDINESH <sthapit.dnsh@gmail.com>
2025-11-09 17:06:06 +0000

    submission: part 3 phonebook exercise 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -76,8 +76,12 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
     const body = request.body
     if (!body.name || !body.number){
-        return response.json({
+        return response.status(400).json({
             "error": "invalid data - missing name or number"
+        })
+    } else if(persons.map(p => p.name).includes(body.name)){
+        return response.status(400).json({
+            "error" : "must be unique"
         })
     }
 
