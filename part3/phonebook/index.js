f56a612756c8d69729d8db5e8afb995de19b8452
sthDINESH <sthapit.dnsh@gmail.com>
2025-11-10 18:08:58 +0000

    submission: part 3 phonebook exercises 3.9-3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -11,6 +11,7 @@ morgan.token('body', function (req, res){
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
