f63a2f2aecb99a75a027a47230ddf39accec01c9
sthDINESH <sthapit.dnsh@gmail.com>
2025-11-10 17:40:32 +0000

    part3 cors setup+port from env

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,6 +10,7 @@ morgan.token('body', function (req, res){
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     { 
@@ -98,7 +100,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=> {
     console.log(`Server running on Port: ${PORT}`)
 })
