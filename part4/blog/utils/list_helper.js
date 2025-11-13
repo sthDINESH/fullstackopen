759662150f087c145d6d5cb7dd1c257c6d5540be
sthDINESH <sthapit.dnsh@gmail.com>
2025-11-13 20:17:26 +0000

    submission: part4 blog exercise 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,9 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => blogs.reduce((total, blog) => total + blog.likes, 0)
+
 module.exports = {
     dummy,
+    totalLikes,
 }
